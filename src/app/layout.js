a1ee17d4889e9db7df9f69b2b32b7b8cc39259c4
NafeesIS <nafeesshawon555@gmail.com>
2023-11-26 00:35:47 +0600

    design complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Next Hero",
-  description: "Next Hero",
+  title: "Burger Website",
+  description: "Burger Website",
 };
 
 const RootLayout = ({ children }) => {
